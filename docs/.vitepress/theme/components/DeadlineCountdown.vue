db31b7b432e97c5bf0839ae9b8bb5708e9403ee5
USTC Student <student@ustc.edu.cn>
2026-04-08 21:33:41 +0800

    修改信息

--- docs/.vitepress/theme/components/DeadlineCountdown.vue
+++ docs/.vitepress/theme/components/DeadlineCountdown.vue
@@ -3,7 +3,7 @@ import { ref, onMounted, onUnmounted } from 'vue'
 
 const countdown = ref('')
 // 设置截止日期：2026年3月30日 23:59:59
-const targetDate = new Date('2026-03-30T23:59:59').getTime()
+const targetDate = new Date('2026-04-20T23:59:59').getTime()
 let timer = null
 
 const updateCountdown = () => {
@@ -52,7 +52,7 @@ onUnmounted(() => {
     <div class="relative z-10">
       <h3 class="!text-white !mt-0 !mb-2 text-lg opacity-90 font-bold flex items-center gap-2">
         <i class="fa-solid fa-hourglass-half animate-pulse"></i>
-        Next Deadline: Lab1 
+        Next Deadline: Lab2
       </h3>
       
       <div class="text-4xl font-mono font-bold mb-2 tracking-tighter">
@@ -61,7 +61,7 @@ onUnmounted(() => {
       
       <div class="flex justify-between items-center text-sm opacity-75">
         <p>Distance to submission</p>
-        <p>Mar 30, 23:59</p>
+        <p>Apr 20, 23:59</p>
       </div>
     </div>
   </div>
