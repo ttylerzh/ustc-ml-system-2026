f75a8e724c8975f1613ca184b91d8aa10ee5e072
USTC Student <student@ustc.edu.cn>
2026-05-12 03:15:30 +0800

    add lec09.pdf

--- docs/.vitepress/theme/components/CourseSchedule.vue
+++ docs/.vitepress/theme/components/CourseSchedule.vue
@@ -56,7 +56,7 @@ const schedule = [
         pdf: '/slides/lec07.pdf' 
   },
   { week: 8, date: 'Apr 28', topic: '第八章 模型量化', lab: 'Lab 3 Released (20%)', video: false, pdf: '/slides/lec08.pdf'  },
-  { week: 9, date: 'May 5', topic: '第九章 机器学习图优化', lab: '', video: true },
+  { week: 9, date: 'May 5', topic: '第九章 机器学习图优化', lab: '', video: false, pdf: '/slides/lec09.pdf'  },
   { week: 10, date: 'May 12', topic: '第十章 大语言模型部署', lab: '', video: true },
   { week: 11, date: 'May 19', topic: '第十一章 大模型后训练', lab: '', video: true },
   { week: 12, date: 'May 26', topic: '第十二章 客座讲座', lab: '', video: true },
